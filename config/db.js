ab41199a8a98a21b7012cd5f9f710d812c14bcf0
hemmylex <65625545+hemmylex@users.noreply.github.com>
2026-07-05 04:19:03 -0400

    Refactor database connection settings
    Updated database connection configuration to use individual environment variables for user, host, database, password, and port.

--- config/db.js
+++ config/db.js
@@ -1,10 +1,16 @@
+// db.js
 import pkg from "pg";
 const { Pool } = pkg;
 
 const pool = new Pool({
-  connectionString: process.env.DATABASE_URL,
+  user: process.env.PG_USER,
+  host: process.env.PG_HOST,
+  database: process.env.PG_DATABASE,
+  password: process.env.PG_PASSWORD,
+  port: process.env.PG_PORT || 5432,
+  // Add this block to fix external cloud connection handshakes
   ssl: {
-    rejectUnauthorized: false,
+    rejectUnauthorized: false, 
   },
 });
 
