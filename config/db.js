09b7da8f5f1276d7582719fdbee3f2ae08ba7ba5
hemmylex <65625545+hemmylex@users.noreply.github.com>
2026-07-05 08:01:17 +0100

    Update db.js

--- config/db.js
+++ config/db.js
@@ -8,6 +8,10 @@ const pool = new Pool({
   database: process.env.PG_DATABASE,
   password: process.env.PG_PASSWORD,
   port: process.env.PG_PORT || 5432,
+  // Add this block to fix external cloud connection handshakes
+  ssl: {
+    rejectUnauthorized: false, 
+  },
 });
 
 export default pool;
