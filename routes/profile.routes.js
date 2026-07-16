6e0550dbdd5c814bc16e32b316c7dde42f82a66b
hemmylex <65625545+hemmylex@users.noreply.github.com>
2026-07-16 04:00:53 +0100

    Delete routes directory

--- routes/profile.routes.js
+++
@@ -1,8 +0,0 @@
-import express from "express";
-import { getProfile, updateProfile } from "../controllers/profile.controller.js";
-import { authMiddleware } from "../middleware/auth.middleware.js"; 
-
-const router = express.Router();
-router.get("/profile", authMiddleware, getProfile);
-router.put("/profile", authMiddleware, updateProfile);
-export default router;
